3b7534f670aed2d4048facf253f2f370ec452efc
Arkasha <Arkasha-94@mail.ru>
2017-01-31 02:20:52 +0200

    Убрал index.php  из .gitignore

---
+++ backend/web/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require(__DIR__ . '/../../autoload.php');
+
+defined('YII_DEBUG') or define('YII_DEBUG', filter_var(getenv('YII_DEBUG'), FILTER_VALIDATE_BOOLEAN));
+defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
+
+require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../common/config/bootstrap.php');
+require(__DIR__ . '/../config/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../common/config/main.php'),
+    require(__DIR__ . '/../config/main.php')
+);
+
+(new yii\web\Application($config))->run();
