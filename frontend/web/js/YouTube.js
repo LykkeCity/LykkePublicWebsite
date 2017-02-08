f7f755e370fc760d4a0d0a0df0d864ae37b17d9a
Arkasha <Arkasha-94@mail.ru>
2017-02-08 18:24:58 +0200

    вернул файл ютуба, исправил баг по другому

--- frontend/web/js/YouTube.js
+++ frontend/web/js/YouTube.js
@@ -6,23 +6,19 @@ function onYouTubeIframeAPIReady() {
         //videoId : 'Le3b6km81uc' - corp,
         videoId : $('#player').data('video-id'),
         height: '390',
-        width: '640'
+        width: '640',
+        events: {
+            'onReady': onPlayerReady,
+        }
     });
+
 }
 
-/*function onPlayerReady(event) {
-
+function onPlayerReady(event) {
     var playButton = document.getElementById("btn_video");
     playButton.addEventListener("click", function() {
         player.playVideo();
         $('.landing--video').addClass('video_played');
     });
 
-}*/
-
-$(document).ready(function () {
-    $('#btn_video').on('click', function () {
-        player.playVideo();
-        $('.landing--video').addClass('video_played');
-    });
-});
+}
