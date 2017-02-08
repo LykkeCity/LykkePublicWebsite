d384eef4e21e803ac0f0ff47dfb4b8be736dad11
Arkasha <Arkasha-94@mail.ru>
2017-02-08 19:20:21 +0200

    поченил репосты в соц. сетях

--- frontend/views/blog/post.php
+++ frontend/views/blog/post.php
@@ -42,8 +42,8 @@ use yii\helpers\Url;
                   <div class="social_share">
                     <div class="social_share__text">Liked this article? Share it!</div>
                     <ul class="social social--simple">
-                      <li><a href="https://www.facebook.com/groups/542506412568917/" target="_blank" class="social__item"><i class="icon icon--fb_simple"></i></a></li>
-                      <li><a href="https://twitter.com/LykkeCity" target="_blank" class="social__item"><i class="icon icon--tw"></i></a></li>
+                      <li><a href="https://www.facebook.com/sharer.php?u=<?=Yii::$app->urlManager->hostInfo.Url::to([$page['url'].'/'.$post['post_url']])?>" target="_blank" class="social__item"><i class="icon icon--fb_simple"></i></a></li>
+                      <li><a href="http://twitter.com/share?text=<?=$post['post_title']?>&url=<?=Yii::$app->urlManager->hostInfo.Url::to([$page['url'].'/'.$post['post_url']])?>" target="_blank" class="social__item"><i class="icon icon--tw"></i></a></li>
                     </ul>
                   </div>
               </div>
