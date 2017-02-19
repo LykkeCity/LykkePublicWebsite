a350b6ea9e40109c42e8e6d6d7306aff66562bcd
Dvin <dvin@MacBook-Air.local>
2017-02-20 01:43:10 +0500

    Add write permissions
    If logs dir not created locally - by default created by root

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,6 @@
 env > /app/public_html/.env
 chown -R www-data:www-data /app/public_html/frontend/web/userfiles
 chown -R www-data:www-data /app/public_html/frontend/web/media
+chown -R www-data:www-data /app/public_html/backend/runtime/logs
 /etc/init.d/apache2 start
 tail -F /var/log/apache2/error.log
