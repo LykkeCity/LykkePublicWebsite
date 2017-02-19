18cde966b906387508e7583fdcc8f08488bbbab7
Dvin <dvin@MacBook-Air.local>
2017-02-19 23:32:24 +0500

    Add write permissions
    For upload

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 env > /app/public_html/.env
+chown -R www-data:www-data /app/public_html/frontend/web/userfiles
+chown -R www-data:www-data /app/public_html/frontend/web/media
 /etc/init.d/apache2 start
 tail -F /var/log/apache2/error.log
