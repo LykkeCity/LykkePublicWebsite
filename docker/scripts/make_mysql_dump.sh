1e9549c0d7a05ea2084562665ba56400151f158f
Dvin <dvin@MacBook-Air.local>
2017-02-20 19:43:26 +0500

    Add comments for dump

--- docker/scripts/make_mysql_dump.sh
+++ docker/scripts/make_mysql_dump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-        echo DB_HOST $DB_HOST
-        echo DB_USER_NAME $DB_USER_NAME
-        echo DB_PASSWORD $DB_PASSWORD
-        echo DB_NAME $DB_NAME
+        echo -- DB_HOST $DB_HOST
+        echo -- DB_USER_NAME $DB_USER_NAME
+        echo -- DB_PASSWORD $DB_PASSWORD
+        echo -- DB_NAME $DB_NAME
         mysqldump -h $DB_HOST -u $DB_USER_NAME -p$DB_PASSWORD $DB_NAME
