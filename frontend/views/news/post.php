6518dbccea9b829eb463563dfbe0e1059f72465d
Arkasha <Arkasha-94@mail.ru>
2017-02-23 17:23:09 +0200

    Убрал лишние ссылки в шаблоне блога и новостей

--- frontend/views/news/post.php
+++ frontend/views/news/post.php
@@ -18,9 +18,7 @@ use yii\helpers\Url;
         <div class="news_article__header">
           <div class="container">
             <div class="news_article__media news_article__media--main">
-              <a href="<?=Url::to([$page['url'].'/'.$post['post_url']])?>">
                 <img src="<?=Yii::$app->request->hostInfo.'/media/news/'.$post['post_img']?>" alt="">
-              </a>
             </div>
           </div>
         </div>
