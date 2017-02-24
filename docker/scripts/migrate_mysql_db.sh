0630d10e158f76bba2bd652697d884cc7aff836a
Dvin <dvyu@yandex.ru>
2017-02-25 01:31:20 +0500

    Fix migration database script

--- docker/scripts/migrate_mysql_db.sh
+++ docker/scripts/migrate_mysql_db.sh
@@ -1,1 +1,6 @@
-/app/public_html/vendor/bin/yii migrate --interactive=0
+#!/bin/sh
+
+cd /app/public_html
+#ln -s console app
+#/app/public_html/vendor/bin/yii migrate --interactive=0
+php /app/public_html/yii migrate --interactive=0
