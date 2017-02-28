b6e6b090384a77f89600243971d1df2ad0b58315
Arkasha <Arkasha-94@mail.ru>
2017-02-28 16:50:27 +0200

    bag fix title home

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -12,6 +12,19 @@ class IndexController extends AppController{
   function actionIndex () {
     $page = SitePages::find()->where(['url' => '/'])->one();
     $this->pageId = $page['id'];
+
+    Yii::$app->view->title = empty($page['title']) ? $page['name'] : $page['title'];
+
+    Yii::$app->view->registerMetaTag([
+      'name' => 'description',
+      'content' =>$page['description']
+    ]);
+
+    Yii::$app->view->registerMetaTag([
+      'name' => 'keywords',
+      'content' =>$page['keywords']
+    ]);
+
     return $this->render('index', ['page' => $page]);
   }
 
