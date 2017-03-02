0d3c5301e0359a5207bcbf0ebf9d0d8430aecb1b
Arkasha <Arkasha-94@mail.ru>
2017-03-02 20:34:35 +0200

    seo правки

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -7,7 +7,7 @@ $params = array_merge(
 $config = [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'bootstrap' => ['log', 'assetsAutoCompress'],
     'controllerNamespace' => 'frontend\controllers',
     'homeUrl' => "/",
     'components' => [
@@ -54,7 +54,38 @@ $config = [
             ],
         ],
 
+      'assetsAutoCompress' =>
+          [
+            'class'                         => '\skeeks\yii2\assetsAuto\AssetsAutoCompressComponent',
+            'enabled'                       => filter_var(getenv('YII_ENV_TEST'), FILTER_VALIDATE_BOOLEAN) ? false : true,
+
+            'readFileTimeout'               => 3,           //Time in seconds for reading each asset file
+
+            'jsCompress'                    => true,        //Enable minification js in html code
+            'jsCompressFlaggedComments'     => true,        //Cut comments during processing js
+
+            'cssCompress'                   => true,        //Enable minification css in html code
+
+            'cssFileCompile'                => true,        //Turning association css files
+            'cssFileRemouteCompile'         => false,       //Trying to get css files to which the specified path as the remote file, skchat him to her.
+            'cssFileCompress'               => true,        //Enable compression and processing before being stored in the css file
+            'cssFileBottom'                 => false,       //Moving down the page css files
+            'cssFileBottomLoadOnJs'         => false,       //Transfer css file down the page and uploading them using js
+
+            'jsFileCompile'                 => true,        //Turning association js files
+            'jsFileRemouteCompile'          => false,       //Trying to get a js files to which the specified path as the remote file, skchat him to her.
+            'jsFileCompress'                => true,        //Enable compression and processing js before saving a file
+            'jsFileCompressFlaggedComments' => true,        //Cut comments during processing js
+
+            'htmlCompress'                  => true,        //Enable compression html
+            'htmlCompressOptions'           =>              //options for compressing output result
+              [
+                'extra' => true,        //use more compact algorithm
+                'no-comments' => true   //cut all the html comments
+              ],
+          ],
     ],
+
     'params' => $params,
     'defaultRoute' => 'index/index'
 ];
