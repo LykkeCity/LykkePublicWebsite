32504fdbb3ed8d249aaef64623c3efead333194b
Anton Shchukin <szwtdy@gmail.com>
2017-03-24 02:24:00 +0500

    HOTfixed: video playbtn

--- frontend/web/js/YouTube.js
+++ frontend/web/js/YouTube.js
@@ -2,13 +2,11 @@ var player;
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-        //videoId : 'h5T2gRGcMso' - home,
-        //videoId : 'Le3b6km81uc' - corp,
         videoId : $('#player').data('video-id'),
         height: '390',
         width: '640',
         events: {
-            'onReady': onPlayerReady,
+            'onReady': onPlayerReady(),
         }
     });
 
@@ -16,9 +14,13 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady(event) {
     var playButton = document.getElementById("btn_video");
-    playButton.addEventListener("click", function() {
-        player.playVideo();
+    playButton.addEventListener("click", function(event) {
+        if(typeof player.playVideo == 'function')
+        {
+            player.playVideo();
+        }
         $('.landing--video').addClass('video_played');
+        event.preventDefault();
     });
 
 }
