2c035faede13e6f0a39008866486583e015e6876
DESKTOP-FP1VGF6\user <sms@ckdigital.ru>
2017-03-24 12:34:10 +0500

    logic for hidden imputs in b2b page

--- frontend/web/js/functions.js
+++ frontend/web/js/functions.js
@@ -226,3 +226,30 @@ $(document).ready(function() {
   initCollapse();
   initForms();
 });
+
+//b2b logic for hidden imputs
+$("#Dr, #Mr, #Mrs, #Ms").change(function () {
+  var titleOptionsArr = document.querySelectorAll('#physics_title > option');
+  for(var i = 0; i < titleOptionsArr.length; i++){
+    titleOptionsArr[i].removeAttribute('selected');
+  };
+
+  var valId = this.id;
+  $("#physics_title option[value=" + valId + "]").attr('selected', 'true');
+});
+
+$("#hear1, #hear2, #hear3, #hear4").change(function () {
+  var leadOptionsArr = document.querySelectorAll('#physics_lead > option');
+  for(var i = 0; i < leadOptionsArr.length; i++){
+    leadOptionsArr[i].removeAttribute('selected');
+  };
+
+  var valValue = this.value;
+  $("#physics_lead option[value='" + valValue + "']").attr('selected', 'true');
+  if(this.id == 'hear4'){
+    $('input#other').removeAttr('disabled');
+  } else{
+    $('input#other').attr('disabled','disabled');
+  }
+
+});
