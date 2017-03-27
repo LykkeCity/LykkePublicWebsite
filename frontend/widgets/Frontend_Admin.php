0852a8ce3c22bcc7e4c0e63668efb93f53e6793f
Anton Shchukin <szwtdy@gmail.com>
2017-03-27 20:55:51 +0500

    disable frontend admin

--- frontend/widgets/Frontend_Admin.php
+++ frontend/widgets/Frontend_Admin.php
@@ -10,17 +10,18 @@ class Frontend_Admin extends Widget
 
     public function run()
     {
-        if (!Yii::$app->user->isGuest
-            && Yii::$app->userAccess->access('edit_frontent') == 1
-        ) {
-            Yii::$app->view->registerCssFile('/css/frontend-admin.css');
-            Yii::$app->view->registerJsFile('/js/frontend-admin.js', [
-                'position' => View::POS_END,
-                'depends'  => 'frontend\assets\MainAsset',
-            ]);
-
-            return $this->render('Frontend_Admin');
-        }
+//        if (!Yii::$app->user->isGuest
+//            && Yii::$app->userAccess->access('edit_frontent') == 1
+//        ) {
+//            Yii::$app->view->registerCssFile('/css/frontend-admin.css');
+//            Yii::$app->view->registerJsFile('/js/frontend-admin.js', [
+//                'position' => View::POS_END,
+//                'depends'  => 'frontend\assets\MainAsset',
+//            ]);
+//
+//            return $this->render('Frontend_Admin');
+//        }
+        return;
     }
 
 }
