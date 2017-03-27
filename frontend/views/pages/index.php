129aae39c6efddc6f3a3269f85193a3af5acac57
Anton Shchukin <szwtdy@gmail.com>
2017-03-27 20:02:21 +0500

    Added: B2B section

--- frontend/views/pages/index.php
+++ frontend/views/pages/index.php
@@ -7,8 +7,8 @@ use \frontend\widgets\SubMenu;
 
 <article class="content">
 
-    <section class="text section--padding">
-        <div class="container">
+    <section class="section--padding section--lead">
+        <div class="container-fluid container--extend">
             <div class="row">
                 <div class="<?=$page['normal_tpl'] == 1
                     ? 'col-sm-8 automargin' : ''?>">
