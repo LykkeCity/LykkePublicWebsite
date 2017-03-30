dad4fe993768097790951edfb130b4998fe1c965
Anton Shchukin <szwtdy@gmail.com>
2017-03-31 00:08:59 +0500

    Fixed: styling

--- frontend/widgets/views/ContactUs.php
+++ frontend/widgets/views/ContactUs.php
@@ -1,7 +1,8 @@
-<nav class="site_nav contact_us_widget">
-    <div class="site_nav__inner">
-        <div class="container">
-            <a href="mailto:accelerator@lykke.com" class="btn btn_contact btn-sm pull-right btn_affix" data-spy="affix"><i class="icon icon--mail"></i> <span>Contact us</span></a>
-        </div>
-    </div>
-</nav>
+<div class="contact_us_widget">
+  <div class="container">
+    <a href="mailto:accelerator@lykke.com"
+       class="btn btn_contact btn-sm pull-right btn_affix affix-top"
+       data-spy="affix" style="right: 15px;"><i class="icon icon--mail"></i>
+      <span class="hidden-xs">Contact us</span></a>
+  </div>
+</div>
