7c12751706233323e7b2386478ebc0c0939c509b
Anton Shchukin <szwtdy@gmail.com>
2017-03-30 22:53:17 +0500

    Fix: b2b form required message

--- frontend/web/js/b2b.js
+++ frontend/web/js/b2b.js
@@ -1,9 +1,10 @@
 //b2b logic for hidden imputs
 $("#Dr, #Mr, #Mrs, #Ms").change(function () {
     var titleOptionsArr = document.querySelectorAll('#physics_title > option');
-    for(var i = 0; i < titleOptionsArr.length; i++){
+    for (var i = 0; i < titleOptionsArr.length; i++) {
         titleOptionsArr[i].removeAttribute('selected');
-    };
+    }
+    ;
 
     var valId = this.id;
     $("#physics_title option[value=" + valId + "]").attr('selected', 'true');
@@ -11,50 +12,50 @@ $("#Dr, #Mr, #Mrs, #Ms").change(function () {
 
 $("#hear1, #hear2, #hear3, #hear4").change(function () {
     var leadOptionsArr = document.querySelectorAll('#physics_lead > option');
-    for(var i = 0; i < leadOptionsArr.length; i++){
+    for (var i = 0; i < leadOptionsArr.length; i++) {
         leadOptionsArr[i].removeAttribute('selected');
-    };
+    }
+    ;
 
     var valValue = this.value;
     $("#physics_lead option[value='" + valValue + "']").attr('selected', 'true');
-    if(this.id == 'hear4'){
+    if (this.id == 'hear4') {
         $('input#other').removeAttr('disabled');
-    } else{
-        $('input#other').attr('disabled','disabled');
+    } else {
+        $('input#other').attr('disabled', 'disabled');
     }
 
 });
 
- var form = document.querySelector('.form_accelerate');
-    form.noValidate = true;
-    form.addEventListener('submit', function(event) {
-        if (!event.target.checkValidity()) {
-            event.preventDefault();
-            for(var i = 0; i < event.srcElement.length;i++){
-                if(event.srcElement[i].value == '' && event.srcElement[i].required == true){
-                    var alert = document.createElement('span');
-                    alert.innerHTML = 'Please fill out this field';
-                    alert.setAttribute('style','color:red;');
-                    alert.setAttribute('class','temp-alert');
-                    
-                    if (event.srcElement[i].parentElement.getAttribute('class') == 'select'){
-                        console.log(event.srcElement[i].parentElement.parentElement.appendChild(alert));
-                    } else{
-                        console.log(event.srcElement[i].parentElement.appendChild(alert));
-                    }
-                    
-                    setTimeout(function() { 
-                        var tempAlert = document.querySelectorAll('.temp-alert');
-                        for(var i = 0; i < tempAlert.length; i++){
-                             tempAlert[i].remove();
-                        }
-                    }, 9000);
+
+var form = document.querySelector('.form_accelerate');
+form.noValidate = true;
+form.addEventListener('submit', function (event) {
+    if (!event.target.checkValidity()) {
+        event.preventDefault();
+        var tempAlert = document.querySelectorAll('.temp-alert');
+        for (var i = 0; i < tempAlert.length; i++) {
+            tempAlert[i].remove();
+        }
+        for (var i = 0; i < event.srcElement.length; i++) {
+            if (event.srcElement[i].value == '' && event.srcElement[i].required == true) {
+                var alert = document.createElement('span');
+                alert.innerHTML = 'Please fill out this field';
+                alert.setAttribute('style', 'color:red;');
+                alert.setAttribute('class', 'temp-alert');
+
+                if (event.srcElement[i].parentElement.getAttribute('class') == 'select') {
+                    console.log(event.srcElement[i].parentElement.parentElement.appendChild(alert));
+                } else {
+                    console.log(event.srcElement[i].parentElement.appendChild(alert));
                 }
+
             }
         }
-    }, false);
+    }
+}, false);
 
-window.onload = function(){
-    $('input[name=COMPLETE_URL]').val(location.origin+'/b2b-thanks');
+window.onload = function () {
+    $('input[name=COMPLETE_URL]').val(location.origin + '/b2b-thanks');
 };
     
