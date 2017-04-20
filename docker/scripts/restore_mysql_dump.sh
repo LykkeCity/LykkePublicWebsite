c5fb2da7c0d0cfd79ef6f8dc230c053c1618b354
Dvin <dvyu@yandex.ru>
2017-04-20 19:04:52 +0500

    Database restore improvements
    Add remove/create database

--- docker/scripts/restore_mysql_dump.sh
+++ docker/scripts/restore_mysql_dump.sh
@@ -4,4 +4,6 @@
         echo DB_USER_NAME $DB_USER_NAME
         echo DB_PASSWORD $DB_PASSWORD
         echo DB_NAME $DB_NAME
+        mysql -h $DB_HOST -u $DB_USER_NAME -p$DB_PASSWORD -e "drop database $DB_NAME;"
+        mysql -h $DB_HOST -u $DB_USER_NAME -p$DB_PASSWORD -e "create database $DB_NAME;"
         mysql -h $DB_HOST -u $DB_USER_NAME -p$DB_PASSWORD $DB_NAME < /dump/lykke-city.sql
