40e031b132bb5e6716db97fa90adaf0351ecd15b
tiunovs <tiunovs@outlook.com>
2017-05-04 23:27:45 +0500

    Advanced TradingView Chart 404 fix

--- frontend/controllers/ExchangeController.php
+++ frontend/controllers/ExchangeController.php
@@ -4,24 +4,41 @@ namespace frontend\controllers;
 use common\models\Asset;
 use common\models\SitePages;
 use Yii;
+use yii\web\View;
 
 class ExchangeController extends AppController
 {
 
-    public function actionIndex()
-    {
+    public function actionIndex() {
         $assets = Asset::find()->asArray()->all();
         return $this->render("index", [
             'assets' => $assets
         ]);
     }
 
-    public function actionRulebook(){
+    public function actionRulebook() {
         return $this->render('rulebook');
     }
 
-    public function actionTerms(){
+    public function actionTerms() {
         return $this->render('terms');
+    }
+
+    public function actionAdvancedChart() {
+        Yii::$app->view->registerJsFile('/js/vendor/tv-charts/charting_library.min.js', [
+            'position' => View::POS_END,
+            'depends'  => 'frontend\assets\MainAsset',
+        ]);
+        Yii::$app->view->registerJsFile('/js/assets/datafeed/datafeed.js', [
+            'position' => View::POS_END,
+            'depends'  => 'frontend\assets\MainAsset',
+        ]);
+        Yii::$app->view->registerJsFile('/js/assets/advanced-chart.js', [
+            'position' => View::POS_END,
+            'depends'  => 'frontend\assets\MainAsset',
+        ]);
+
+        return $this->render('advanced-chart');
     }
 
 }
