5e3df8059030d2a49cb8ec927eec0be08af28cca
Anton Shchukin <szwtdy@gmail.com>
2017-07-01 16:59:06 +0500

    change retargeting view

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -73,24 +73,6 @@ use yii\helpers\Html;
         <? } ?>
 
 
-      <!-- Код тега ремаркетинга Google -->
-      <!--------------------------------------------------
-      С помощью тега ремаркетинга запрещается собирать информацию, по которой можно идентифицировать личность пользователя. Также запрещается размещать тег на страницах с контентом деликатного характера. Подробнее об этих требованиях и о настройке тега читайте на странице http://google.com/ads/remarketingsetup.
-      --------------------------------------------------->
-      <script type="text/javascript">
-        /* <![CDATA[ */
-        var google_conversion_id = 976584301;
-        var google_custom_params = window.google_tag_params;
-        var google_remarketing_only = true;
-        /* ]]> */
-      </script>
-      <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-      </script>
-      <noscript>
-        <div style="display:inline;">
-          <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/976584301/?guid=ON&amp;script=0"/>
-        </div>
-      </noscript>
 
 
         <!-- Start of lykkex Zendesk Widget script -->
@@ -151,6 +133,31 @@ use yii\helpers\Html;
 
     <?=$content?>
     <?php $this->endBody() ?>
+
+
+    <!-- Код тега ремаркетинга Google -->
+    <!--------------------------------------------------
+    С помощью тега ремаркетинга запрещается собирать информацию, по которой можно идентифицировать личность пользователя. Также запрещается размещать тег на страницах с контентом деликатного характера. Подробнее об этих требованиях и о настройке тега читайте на странице http://google.com/ads/remarketingsetup.
+    --------------------------------------------------->
+    <script type="text/javascript">
+      /* <![CDATA[ */
+      var google_conversion_id = 976584301;
+      var google_custom_params = window.google_tag_params;
+      var google_remarketing_only = true;
+      /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+    </script>
+    <noscript>
+      <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/976584301/?guid=ON&amp;script=0"/>
+      </div>
+    </noscript>
+    <style>
+      iframe[name=google_conversion_frame] {
+        display: none;
+      }
+    </style>
 
     </body>
 </html>
